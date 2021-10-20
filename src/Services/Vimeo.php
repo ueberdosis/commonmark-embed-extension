027af76755b41b0ecbfd68b8c260d7af45795aba
Hans Pagel <hans.pagel@ueber.io>
2021-10-20 11:31:34 +0200

    add support for Vimeo private URLs, make Vimeo embed responsive

--- src/Services/Vimeo.php
+++ src/Services/Vimeo.php
@@ -8,22 +8,34 @@ use Ueberdosis\CommonMark\ServiceInterface;
 
 class Vimeo implements ServiceInterface
 {
-    public const pattern = '(http|https)?:\/\/(www\.|player\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|video\/|)(\d+)(?:|\/\?)';
+    public const pattern = '(http|https)?:\/\/(www\.|player\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|video\/|)(\d+)(?:|\/\?)\/?([a-z0-9]*)?';
 
     public function render(Embed $node): HtmlElement
     {
-        // Output: <iframe src="https://player.vimeo.com/video/551694700" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+        // Output: <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/551694700" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+
+        $id = $this->getId($node->getUrl());
+        if ($hash = $this->getHash($node->getUrl())) {
+            $query = '?' . http_build_query([
+                'h' => $hash,
+            ]);
+        }
 
         return new HtmlElement(
-            'iframe',
+            'div',
             [
-                'src' => 'https://player.vimeo.com/video/' . $this->getId($node->getUrl()),
-                'width' => '640',
-                'height' => '360',
-                'frameborder' => '0',
-                'allow' => 'autoplay; fullscreen; picture-in-picture',
-                'allowfullscreen' => '',
+                'style' => 'padding:56.25% 0 0 0;position:relative;',
             ],
+            new HtmlElement(
+                'iframe',
+                [
+                    'src' => 'https://player.vimeo.com/video/' . $id . ($query ?? null),
+                    'frameborder' => '0',
+                    'allow' => 'autoplay; fullscreen; picture-in-picture',
+                    'allowfullscreen' => '',
+                    'style' => 'position:absolute;top:0;left:0;width:100%;height:100%;',
+                ],
+            )
         );
     }
 
@@ -32,5 +44,12 @@ class Vimeo implements ServiceInterface
         preg_match('/'.self::pattern.'/', $url, $matches);
 
         return $matches[4] ?? '';
+    }
+
+    protected function getHash($url)
+    {
+        preg_match('/'.self::pattern.'/', $url, $matches);
+
+        return $matches[5] ?? '';
     }
 }
--- tests/VimeoTest.php
+++ tests/VimeoTest.php
@@ -32,7 +32,33 @@ https://vimeo.com/551694700
 MARKDOWN;
 
         $this->assertEquals((string) $converter->convertToHtml($markdown), <<<HTML
-<iframe src="https://player.vimeo.com/video/551694700" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen=""></iframe>
+<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/551694700" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+
+HTML);
+    }
+
+    /** @test */
+    public function private_vimeo_url_is_transformed_to_an_embed()
+    {
+        // Configure the Environment with all the CommonMark parsers/renderers
+        $environment = new Environment([
+            'embeds' => [
+                new Vimeo(),
+            ],
+        ]);
+        $environment->addExtension(new CommonMarkCoreExtension());
+
+        // Add this extension
+        $environment->addExtension(new EmbedExtension());
+
+        // Instantiate the converter engine and start converting some Markdown!
+        $converter = new MarkdownConverter($environment);
+        $markdown = <<<MARKDOWN
+https://vimeo.com/551694700/e6d06c6e21
+MARKDOWN;
+
+        $this->assertEquals((string) $converter->convertToHtml($markdown), <<<HTML
+<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/551694700?h=e6d06c6e21" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
 
 HTML);
     }
